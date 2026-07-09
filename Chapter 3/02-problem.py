30f137be0da35bad370d7ca221d10703ff308f95
jahiralom07-dotcom <jahiralom07@gmail.com>
2026-07-09 10:02:42 +0530

    Add files via upload

---
+++ Chapter 3/02-problem.py
@@ -0,0 +1,5 @@
+letter = """Dear <|Name|>
+You are selected
+<|Dete|>"""
+
+print(letter.replace("<|Name|>","Jahir").replace("<|Dete|>","13 july 2027").title())
---
+++ Chapter 3/str_functn.py
@@ -0,0 +1,12 @@
+name = "jahhir alom is a good boy"
+
+print(len(name))
+print(name.endswith("hir"))
+print(name.startswith("jah"))
+print(name.capitalize())
+print(name.title())
+print(name.replace("good","bad"))
+print(name.find("h"))
+print(name.swapcase())
+print(name.split())
+# print(name.join(['jahhir','alom','is','a','good','boy']))
