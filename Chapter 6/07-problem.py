d8a7bd22e992830a783059658b92ac3a9a2572b9
jahiralom07-dotcom <jahiralom07@gmail.com>
2026-07-09 10:08:32 +0530

    Add files via upload

---
+++ Chapter 6/07-problem.py
@@ -0,0 +1,7 @@
+post = input("Enter the post: ")
+
+if("Jahir".lower() in post.lower()):
+    print("This post is talking about jahir")
+
+else:
+    print("This post id not talking about jahir")
---
+++ Chapter 6/08-problem.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if(18>=age>=18):
+    print("You are eligible for vote")
+
+
+else:
+    print("You are not eligible for vote")
+
+print("Thank you!!")
